ccd34f0752a55a9e0b7a8534c760869d406db670
Prasit Kumar Dutta <60533793+p-ship-it@users.noreply.github.com>
2020-06-10 07:13:08 +0530

    Add files via upload

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.onTap, @required this.buttonTile});
+
+  final Function onTap;
+  final String buttonTile;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTile,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
